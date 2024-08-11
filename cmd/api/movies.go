1e5c347d8fd091e23cb1d849edcb4323c4f5eba6
Subash Giri <mrsbs.giri@gmail.com>
2024-08-11 21:46:39 +0545

    feat: create movie fields validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"greenlight-api/internal/data"
+	"greenlight-api/internal/validator"
 	"net/http"
 	"time"
 )
@@ -18,6 +19,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
